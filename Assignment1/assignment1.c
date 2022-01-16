9a9913187a87ce050ddecdce31fc561ff6f498c8
ramesh720 <43946769+ramesh720@users.noreply.github.com>
2022-01-16 20:20:29 +0530

    Update assignment1.c

--- Assignment1/assignment1.c
+++ Assignment1/assignment1.c
@@ -1,20 +1,38 @@
 
+
+// Question. State any one absorption law in boolean algebra and verify using truth table
+
 #include<stdio.h>
 
 int main()
 {
   
-unsigned char  x=0x00,y=0x01;
+unsigned char  x=0x00,y=0x00,i;
   
 unsigned char one = 0x01;
 unsigned char z,z2;
   
-z = x & ( x | y );
-z2 = x | ( x & y );
-printf("x=%x,y=%x\n",one&x,one&y);
-printf("output for law1 = %x\n" ,one&z);
-printf("output for law2 = %x\n" ,one&z2);
+//law 1 = x ( x + y)
 
+for (i= 0x00; i<0x04; i++)
+{ 
+
+x = i>>1;
+y = i>>0;
+
+z = x & ( x | y );
+
+//law 2 = x + xy
+
+z2 = x | ( x & y );
+
+
+printf("inputs x=%x,y=%x   ",one&x,one&y);
+printf("x = %x  " ,one&z2);
+printf("law1 = %x  " ,one&z);
+printf("law2 = %x\n" ,one&z2);
+
+}
 return 0;
 
 }
